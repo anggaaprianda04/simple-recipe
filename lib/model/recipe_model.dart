de741ddf38d7b5c92f6b3095dd1a6c07043c85a2
anggaaprianda04 <anggaaprianda04@gmail.com>
2023-03-09 16:41:36 +0700

    add package and file service

--- lib/model/recipe_model.dart
+++ lib/model/recipe_model.dart
@@ -1,1 +1,22 @@
-class RecipeModel {}
+class RecipeModel {
+  String? name;
+  String? image;
+  String? totalTime;
+  int? rating;
+
+  RecipeModel({
+    required this.name,
+    required this.image,
+    required this.totalTime,
+    required this.rating,
+  });
+
+  factory RecipeModel.formJson(Map<String, dynamic> json) {
+    return RecipeModel(
+      name: json["name"],
+      image: json["images"][0]["hostedLargeUrl"] as String,
+      totalTime: json["totalTime"] as String,
+      rating: json["rating"],
+    );
+  }
+}
---
+++ lib/service/recipe_service.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'package:simple_ecipe/model/recipe_model.dart';
+
+class RecipeService {
+  // Future<List<RecipeModel>> getRecipe() async {}
+}
